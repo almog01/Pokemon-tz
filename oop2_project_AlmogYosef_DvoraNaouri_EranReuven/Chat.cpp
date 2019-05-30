43506cc26a0fa3d191e40f3a0ceb397b687548e1
Dvora Naouri <dvorana@std.hac.ac.il>
2019-05-30 13:57:57 +0300

    all

--- oop2_project_AlmogYosef_DvoraNaouri_EranReuven/Chat.cpp
+++ oop2_project_AlmogYosef_DvoraNaouri_EranReuven/Chat.cpp
@@ -10,13 +10,10 @@ Chat::Chat(const Vector2f & pos, const Vector2f & size, string text)
 	m_chatWindow.setOrigin(m_chatWindow.getGlobalBounds().width / 2, m_chatWindow.getGlobalBounds().height / 2);
 }
 
-Chat::ch;
-m_text.setString(text);							
-m_text.setFont(rm.getFont());						
-m_text.setCharacterSize(btnSize - TEXT_PADDING);	// text_size = button_size - padding (to make the text "inside" the button)
-m_text.setFillColor(sf::Color::Red);				// color = red
-m_text.setPosition(m_button.getPosition());			// same position as the button
-m_text.setOrigin(m_text.getGlobalBounds().width / 2, m_text.getGlobalBounds().width / 2);	// set origin to the middle
+// Chat::ch;
+//m_text.setFont(rm.getFont());						
+//m_text.setFillColor(sf::Color::Red);				// color = red
+//m_text.setOrigin(m_text.getGlobalBounds().width / 2, m_text.getGlobalBounds().width / 2);	// set origin to the middle
 
 
 Chat::~Chat()
