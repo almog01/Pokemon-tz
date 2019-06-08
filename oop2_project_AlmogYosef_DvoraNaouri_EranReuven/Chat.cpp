905dce630c741cd2099ddba815e7ef0244d34c53
Almog Yosef <almog64y@gmail.com>
2019-06-08 21:59:55 +0300

    Added working chat

--- oop2_project_AlmogYosef_DvoraNaouri_EranReuven/Chat.cpp
+++ oop2_project_AlmogYosef_DvoraNaouri_EranReuven/Chat.cpp
@@ -1,12 +1,108 @@
 #include "Chat.h"
+#include <sstream>
 
+using sf::Vector2f;
+using sf::VideoMode;
+using sf::Keyboard;
+using std::istringstream;
 
+const unsigned TEXT_SIZE = 15U;
+const float MARGIN_LEFT = 11.f;
+const float MARGIN_TOP = 4.f;
+const size_t MAX_LINE = 39;
 
-Chat::Chat()
+Chat::Chat(const RenderWindow & window, const string & chat)
 {
+	// initialize chat window
+	m_chatWindow.setTexture(Resource::texture("chat_window"), true);
+	m_chatWindow.setOrigin(m_chatWindow.getGlobalBounds().width / 2, m_chatWindow.getGlobalBounds().height / 2);
+	auto view = window.getView();
+	m_chatWindow.setPosition(Vector2f(view.getCenter().x, view.getCenter().y + (view.getSize().y / 2.5f)));
+
+	// initialize text
+	m_text.setFont(Resource::font);
+	m_text.setCharacterSize(TEXT_SIZE);
+	m_text.setFillColor(sf::Color::Black);
+	auto windowBounds = m_chatWindow.getGlobalBounds();
+	m_text.setPosition(Vector2f(windowBounds.left + MARGIN_LEFT, windowBounds.top + MARGIN_TOP));
+	
+	// set chat
+	setChat(chat);
 }
 
 
 Chat::~Chat()
 {
+}
+
+void Chat::draw(RenderWindow & window)
+{
+	window.draw(m_chatWindow);
+	window.draw(m_text);
+}
+
+void Chat::setChat(const string & text)
+{
+	istringstream iss(text);
+	string word;
+	string line;
+	string sentence;
+	int counter = 0;
+
+	while (iss >> word)
+	{
+		if (counter % 2 == 0)
+		{
+			if (line.length() + word.length() > MAX_LINE)
+			{
+				sentence += line;
+				sentence += '\n';
+				line.clear();
+				line += word;
+				counter++;
+			}
+			else
+				line += word;
+
+			line += ' ';
+		}
+		else
+		{
+			if (line.length() + word.length() > MAX_LINE)
+			{
+				sentence += line;
+				m_chat.push_back(sentence);
+				sentence.clear();
+				line.clear();
+				line += word;
+				counter++;
+			}
+			else
+				line += word;
+
+			line += ' ';
+		}
+	}
+	if (!line.empty())
+		sentence += line;
+	if (!sentence.empty())
+		m_chat.push_back(sentence);
+
+	m_it = m_chat.cbegin();
+	m_text.setString(*m_it);
+}
+
+void Chat::keyReleasedHandler(const Event & event)
+{
+	switch (event.key.code)
+	{
+	case Keyboard::X:		// x button pressed
+	case Keyboard::Z:		// or z button pressed
+		++m_it;
+		if (m_it != m_chat.cend())
+			m_text.setString(*m_it);
+		else
+			m_isFinished = true;
+		break;
+	}
 }
--- oop2_project_AlmogYosef_DvoraNaouri_EranReuven/Chat.h
+++ oop2_project_AlmogYosef_DvoraNaouri_EranReuven/Chat.h
@@ -1,9 +1,27 @@
 #pragma once
 #include "Screen.h"
+
+using sf::Sprite;
+using sf::Text;
+using std::string;
+using std::vector;
+
 class Chat : public Screen
 {
 public:
-	Chat();
+	Chat(const RenderWindow & window, const string & chat);
 	~Chat();
+
+	// Inherited via Screen
+	virtual void draw(RenderWindow & window);
+	virtual void keyReleasedHandler(const Event & event) override;
+
+	void setChat(const string & text);
+
+private:
+	Sprite m_chatWindow;
+	Text m_text;
+	vector<string> m_chat;
+	vector<string>::const_iterator m_it;
 };
 
